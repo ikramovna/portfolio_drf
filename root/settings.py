94a37fcc62c1999403282f9f3bb720e3f0c59433
ZokirjonovaMuslima <muslimazokirjonova2004@gmail.com>
2023-07-20 15:53:02 +0500

    Some changes

--- root/settings.py
+++ root/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'shohsana.me']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 MY_APPS = [
     'apps.users',
