1f5c263659709464a79a34a6b74e1e86f6d48441
muslima <muslimazokirjonova2004@gmail.com>
2023-10-24 21:53:52 +0500

    Initial commit

--- root/urls.py
+++ root/urls.py
@@ -9,13 +9,13 @@ from root.settings import MEDIA_URL, MEDIA_ROOT, STATIC_URL, STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('apps.users.urls')),
+    # path('user/', include('apps.users.urls')),
     path('portfolio/', include('apps.portfolio.urls')),
     path('', include('apps.send_email.urls')),
 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 
 ] + swagger_urls + static(MEDIA_URL, document_root=MEDIA_ROOT) + static(STATIC_URL, document_root=STATIC_ROOT)
