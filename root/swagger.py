66bf36fa09182a1878ac14a8e78802bb491f1855
muslima <muslimazokirjonova2004@gmail.com>
2023-10-24 18:18:18 +0500

    iltimos

--- root/swagger.py
+++ root/swagger.py
@@ -5,7 +5,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Portfolio with musa API",
+        title="Portfolio API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
